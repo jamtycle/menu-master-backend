a8e9bf9f2d43f3e5d277f63cd3e42b574eb14ebc
Bruno Ramirez <bramirez206@hotmail.com>
2024-03-30 23:47:44 -0400

    fixing typo on CORS

--- src/config/cors.rs
+++ src/config/cors.rs
@@ -19,7 +19,7 @@ impl Fairing for CORS {
         response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
         response.set_header(Header::new(
             "Access-Control-Allow-Methods",
-            "POST, GET, DELETE, PUT, OPTION",
+            "POST, GET, DELETE, PUT, OPTIONS",
         ));
         response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
         response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
